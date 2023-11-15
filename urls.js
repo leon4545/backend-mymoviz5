5dc68bbfdd4934ebefccf4c6b895b11b8c19aadf
Leon Schouller <leonalexandreschouller@gmail.com>
2023-11-15 17:38:09 +0100

    urlmises

--- urls.js
+++ urls.js
@@ -1,6 +1,6 @@
 // Insert the backend URL given by Vercel in this variable
-const BACKEND_URL = 'https://backend-mymoviz5-737lurdd5-leon-schoullers-projects.vercel.app'
+const BACKEND_URL = 'https://backend-mymoviz5-737lurdd5-leon-schoullers-projects.vercel.app/movies'
 
 // Insert the frontend URL given by Vercel in this variable
-const FRONTEND_URL = 'https://frontend-mymoviz5-fpj9gt4ni-leon-schoullers-projects.vercel.app'
+const FRONTEND_URL = 'https://frontend-mymoviz5-fpj9gt4ni-leon-schoullers-projects.vercel.app/movies'
 module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
